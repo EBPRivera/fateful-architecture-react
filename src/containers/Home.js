9b1950363c12c81efd39a2b20711663f15c4a70c
Eduard Bryan P. Rivera <edubprivera@yahoo.com>
2023-08-17 16:35:37 +0800

    Refactor home and skills page boilerplate

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,37 @@
+import { Container } from "react-bootstrap";
+
 const Home = () => {
   return (
-    <div id="home">
-      <h1>Hello Siphoned Aether</h1>
-      <p>A container for the home page</p>
+    <div id="home-page">
+      <div>
+        <Container>
+          <h3 className="py-3 mb-0">Welcome to Scaena</h3>
+        </Container>
+      </div>
+      <div>
+        <Container>
+          <p className="px-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            condimentum fringilla nibh et commodo. Suspendisse at rhoncus neque,
+            vitae condimentum nibh. Nam et erat sed augue sodales lacinia
+            vulputate a tellus. Nam mollis odio ac urna hendrerit, vitae
+            vehicula purus imperdiet. In hac habitasse platea dictumst. Sed
+            feugiat nec diam sit amet tincidunt. Morbi dignissim urna eget sem
+            venenatis congue.
+          </p>
+          <p className="px-0">
+            Suspendisse lacinia tristique risus, eget placerat sapien accumsan
+            non. Curabitur tincidunt, dolor sit amet posuere rutrum, enim dolor
+            tincidunt est, laoreet condimentum lacus mauris non sapien. Donec ac
+            nisl vel ipsum commodo iaculis eu ut arcu. Pellentesque eleifend
+            vehicula massa eget porttitor. Sed porta quis mauris sit amet
+            dignissim. Pellentesque id neque sed odio mollis convallis. Nulla
+            nec odio lorem. Etiam ornare efficitur aliquet. Curabitur non
+            maximus urna, sed lacinia neque. Duis neque arcu, interdum vitae
+            sodales eu, iaculis a nulla.
+          </p>
+        </Container>
+      </div>
     </div>
   );
 };
